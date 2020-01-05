1be6ab18e045b02c81a78f251e6d462ab81f0788
JeffChen001218 <1712482297@qq.com>
2020-01-05 17:40:36 +0800

    First commit

---
+++ app/src/main/java/com/miniweather/android/util/HttpUtil.java
@@ -0,0 +1,7 @@
+package com.miniweather.android.util;
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address , okhttp3.Callback callback){
+
+    }
+}
---
+++ app/src/main/java/com/miniweather/android/util/dd.java
@@ -0,0 +1,4 @@
+package com.miniweather.android.util;
+
+public class dd {
+}
